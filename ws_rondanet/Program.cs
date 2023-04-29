c16d8350646e1ea6d5e436eb694a12e835ae1392
virgiliomca <virgiliomca@gmail.com>
2023-04-29 16:03:43 -0400

    Protección inicial

---
+++ ws_rondanet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ws_rondanet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                wsRondanet.RondanetClient ws = new wsRondanet.RondanetClient();
+                //recuperamo los pedidos del WS de Rondanet
+                string[] sRespuesta = ws.obtenerMensajes("user", "pass", "xml");
+                Console.WriteLine(sRespuesta[0]);
+                Console.ReadLine();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
